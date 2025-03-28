d607e7d2c2039c0b34327196ca2ce0509d2272a1
Misha Causur <mishacausur@mail.ru>
2025-03-28 21:15:02 +0500

    added recycler view

---
+++ app/src/main/java/ru/netology/nmedia/adapter/PostAdapter.kt
@@ -0,0 +1,24 @@
+package ru.netology.nmedia.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PostAdapter: RecyclerView.Adapter<PostViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    fun get(): LiveData<Post>
-    fun like()
-    fun share()
+    fun getAll(): LiveData<List<Post>>
+    fun like(postId: UInt)
+    fun share(postId: UInt)
 }
