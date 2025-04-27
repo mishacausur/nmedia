59a50bea82012d722d66706a8655e914f256bbb2
mishacausur <mishacausur@mail.ru>
2025-04-27 19:20:24 +0500

    handled like

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -4,13 +4,37 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 
-class FCMService: FirebaseMessagingService() {
+class FCMService : FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
-        println(Gson().toJson(message))
+        message.data["action"]?.let {
+            when (Action.valueOf(it)) {
+                Action.LIKE -> handleLike(
+                    Gson().fromJson(
+                        message.data["content"],
+                        Like::class.java
+                    )
+                )
+            }
+        }
     }
 
     override fun onNewToken(token: String) {
         println(token)
     }
+
+    private fun handleLike(like: Like) {
+
+    }
 }
+
+enum class Action {
+    LIKE
+}
+
+data class Like(
+    val userId: Int.Companion,
+    val userName: String,
+    val postId: Int,
+    val postAuthor: String
+)
