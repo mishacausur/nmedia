e36234b13aafb84b751e6523df6d553535fb9946
mishacausur <mishacausur@mail.ru>
2025-07-08 21:05:22 +0500

    flow started

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,10 +1,10 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
+import kotlinx.coroutines.flow.Flow
 import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
-    val data: LiveData<List<Post>>
+    val data: Flow<List<Post>>
     suspend fun like(postId: Long, isLiked: Boolean): Post
     suspend fun share(postId: Long)
     suspend fun remove(id: Long)
