dfdf8a4a26de910cbe8084e072de0742cb45d7a2
mishacausur <mishacausur@mail.ru>
2025-10-02 16:43:41 +0500

    token added

---
+++ app/src/main/java/ru/netology/nmedia/auth/AppAuth.kt
@@ -0,0 +1,50 @@
+package ru.netology.nmedia.auth
+
+import android.content.Context
+import androidx.core.content.edit
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import ru.netology.nmedia.dto.Token
+
+class AppAuth private constructor(context: Context) {
+
+    private companion object {
+        const val ID_KEY = "ID_KEY"
+        const val TOKEN_KEY = "TOKEN_KEY"
+    }
+
+    private val preferences = context.applicationContext.getSharedPreferences(
+        "auth",
+        Context.MODE_PRIVATE
+    )
+
+    private val _data: MutableStateFlow<Token?>
+
+    init {
+        val _id = preferences.getLong(ID_KEY, 0L)
+        val _token = preferences.getString(TOKEN_KEY, null)
+
+        if (_id == 0L || _token == null) {
+            preferences.edit { clear() }
+            _data = MutableStateFlow(null)
+        } else {
+            _data = MutableStateFlow(Token(_id, _token))
+        }
+    }
+
+    val data = _data.asStateFlow()
+
+    fun setAuth(id: Long, token: String) {
+        preferences.edit {
+            putLong(ID_KEY, id)
+            putString(TOKEN_KEY, token)
+        }
+
+        _data.value = Token(id, token)
+    }
+
+    fun unauth() {
+        preferences.edit { clear() }
+        _data.value = null
+    }
+}
